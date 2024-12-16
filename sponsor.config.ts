e22b4d36653e1b379af94f55644f18b57d0a1fe5
Aaron <aaronklinker1@gmail.com>
2024-12-16 14:11:12 -0600

    Initial commit

---
+++ sponsor.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, tierPresets } from "sponsorkit";
+
+export default defineConfig({
+  tiers: [
+    { title: "Past Sponsors", monthlyDollars: -1, preset: tierPresets.xs },
+    { title: "Backers" },
+    { title: "Sponsors", monthlyDollars: 10, preset: tierPresets.medium },
+    { title: "Silver Sponsors", monthlyDollars: 50, preset: tierPresets.large },
+    { title: "Gold Sponsors", monthlyDollars: 100, preset: tierPresets.xl },
+  ],
+  sponsorsAutoMerge: true,
+  renders: [
+    { name: "sponsors", width: 800, formats: ["svg", "png"] },
+    { name: "sponsors-wide", width: 1200, formats: ["svg"] },
+  ],
+});
